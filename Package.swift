c1a5cb9a1042353853183242f8411076d3817e02
Juanita Vyatri <juanita.vyatri@gmail.com>
2024-06-11 09:56:17 +0700

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "CCHDarwinNotificationCenter",
+    platforms: [
+        .iOS(.v7),
+        .macOS(.v10_9)
+    ],
+    products: [
+        .library(
+            name: "CCHDarwinNotificationCenter",
+            targets: ["CCHDarwinNotificationCenter"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "CCHDarwinNotificationCenter",
+            path: "CCHDarwinNotificationCenter",
+            sources: ["CCHDarwinNotificationCenter"],
+            publicHeadersPath: "."
+        )
+    ]
+)
